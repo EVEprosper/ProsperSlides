5bae291c681972aa7b1c56f7919155d820c5f8db
Lockefox <locke.renard@gmail.com>
2017-01-15 18:25:23 -0500

    adding new isotopes plot for show

---
+++ ProsperSlides/R_templates/fuel_isotopes_chart.r
@@ -0,0 +1,144 @@
+plot.width = {img_width}
+plot.height = {img_height}
+plot.range = {date_range}
+plot.locationid = {locationid}
+plot.title = '{plot_title}'
+plot.path = '{plot_path}'
+
+date.min <- Sys.Date() - plot.range
+type_list <- c(16274, 17887, 17888, 17889)
+plot.typeids.str <- paste(type_list, collapse=',')
+## Event List ##
+event.query <- paste0(
+    'SELECT datetime, eventName ',
+    'FROM event_list ',
+    'WHERE eventGroup in (\'patch\') ',
+    'AND datetime > \'', date.min, '\' '
+)
+event <- sqlQuery(emd, event.query)
+event$datetime <- as.POSIXlt(event$datetime, tz='GMT')
+do_lines <- TRUE
+if(nrow(event)==0){{
+    do_lines <- FALSE
+}}
+
+## Get Data ##
+ec.query <- paste0(
+    'SELECT price_date AS `date`, price_time AS `hour`, locationid, typeid, ',
+    'SUM(IF(buy_sell=1, price_best,0)) AS `SellOrder` ',
+    'FROM snapshot_evecentral ',
+    'WHERE locationid=', plot.locationid, ' ',
+    'AND typeid IN (', plot.typeids.str, ') ',
+    'AND price_date > \'', date.min, '\' ',
+    'GROUP BY price_date, price_time, typeid'
+)
+ec <- sqlQuery(emd, ec.query)
+
+odbcClose(emd)
+ec$date <- as.Date(ec$date)
+ec$typeid <- as.factor(ec$typeid)
+ec <- subset(ec, SellOrder > 0)
+ec$datetime <- paste(ec$date, ec$hour, sep=' ')
+ec$datetime <- as.POSIXlt(ec$datetime, tz='GMT')
+
+## CREST Lookups ##
+CREST_BASE = 'https://crest-tq.eveonline.com/'
+solarsystem.addr <- paste0(CREST_BASE, 'solarsystems/', plot.locationid, '/')
+solarsystem.json <- fromJSON(readLines(solarsystem.addr))
+solarsystem.name <- solarsystem.json$name
+
+ec$locationName <- solarsystem.name
+
+type_list <- unique(ec$typeid)
+ec$typeName <- NA
+for(type.index in 1:length(type_list)){{
+    type.name <- ''
+    type.id <- type_list[type.index]
+    type.addr <- paste0(CREST_BASE, 'inventory/types/', type.id, '/')
+    type.json <- fromJSON(readLines(type.addr))
+    type.name <- type.json$name
+    ec$typeName[ec$typeid==type.id] <- type.name
+}}
+
+## Plot Theme ##
+theme_dark <- function( ... ) {{
+  theme(
+    text = element_text(color="gray90"),
+    title = element_text(size=rel(2.5),hjust=0.05,vjust=3.5),
+    axis.title.x = element_text(size=rel(0.75),hjust=0.5, vjust=0),
+    axis.title.y = element_text(size=rel(0.75),hjust=0.5, vjust=1.5),
+    plot.margin = unit(c(2,1,1,1), "cm"),
+    plot.background=element_rect(fill="gray8",color="gray8"),
+    panel.background=element_rect(fill="gray10",color="gray10"),
+    panel.grid.major = element_line(colour="gray17"),
+    panel.grid.minor = element_line(colour="gray12"),
+    axis.line = element_line(color = "gray50"),
+    plot.title = element_text(color="gray80"),
+    axis.title = element_text(color="gray70"),
+    axis.text = element_text(color="gray50",size=rel(1.1)),
+    legend.key = element_rect(fill="gray8",color="gray8"),
+    legend.background = element_rect(fill="gray8"),
+    legend.title = element_text(size=rel(0.6)),
+    legend.text = element_text(size=rel(1.1)),
+    strip.background = element_rect(fill="#252525"),
+    strip.text = element_text(size=rel(1.2))
+  ) + theme(...)
+}}
+
+uniqueID <- unique(ec$typeid)
+ec$SellOrder.smooth <- NA
+for(id in uniqueID){{
+  prices <- ec$SellOrder[ec$typeid==id]
+  smooth <- rollmean(prices, k=24, align='center')
+  ec$SellOrder.smooth[ec$typeid==id] <- smooth
+}}
+plot.data <- ec
+
+## Plot Data ##
+plot <- ggplot(
+  plot.data,
+  aes(
+    x=datetime,
+    y=SellOrder.smooth,
+    color=typeName)
+)
+plot <- plot +geom_line(
+  size=1.5
+)
+plot <- plot + theme_dark()
+plot <- plot + labs(
+  title=plot.title,
+  fill='Item Name',
+  x='date',
+  y='price'
+)
+if(do_lines){{
+  plot <- plot + geom_vline(
+    xintercept=as.numeric(event$datetime),
+    linetype=2,
+    color='white'
+  )
+  plot <- plot + geom_text(
+    aes(
+      x=datetime,
+      y=Inf,
+      label=eventName),
+    color='white',
+    angle=-90,
+    vjust=1.2,
+    hjust=0,
+    data=event,
+    inherit.aes=FALSE
+  )
+}}
+plot <- plot + scale_y_continuous(
+  position='right'
+)
+plot <- plot + scale_color_manual(
+  values=c(
+    "Oxygen Isotopes"="#097686",
+    "Hydrogen Isotopes"="#B7090D",
+    "Nitrogen Isotopes"="#2169E0",
+    "Helium Isotopes"="#EA8B25")
+)
+print(plot)
